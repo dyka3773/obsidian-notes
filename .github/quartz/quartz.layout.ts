d797d3e8baf55731eccf15d056b7ceea17794bdf
Hercules Konsoulas <dyka3773@gmail.com>
2025-03-14 17:16:13 +0100

    vault automatic backup: 2025-03-14 17:16:13

--- .github/quartz/quartz.layout.ts
+++ .github/quartz/quartz.layout.ts
@@ -24,8 +24,15 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
