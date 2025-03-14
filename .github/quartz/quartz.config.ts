1307e6ea9c8c55958d1aa0af645b46eee812259f
Hercules Konsoulas <dyka3773@gmail.com>
2025-03-14 13:37:07 +0100

    vault automatic backup: 2025-03-14 13:37:07

--- .github/quartz/quartz.config.ts
+++ .github/quartz/quartz.config.ts
@@ -26,14 +26,15 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#F5F4F5",
-          lightgray: "#F9DF9B",
-          gray: "#FDCA40",
-          darkgray: "#836924",
-          dark: "#080708",
-          secondary: "#1098F7",
-          tertiary: "#379634",
-          highlight: "rgba(16, 152, 247, 0.15)",
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
         },
         darkMode: {
           light: "#161618",
